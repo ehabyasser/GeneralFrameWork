d6433e185b0e75708b40ae112f829d88fff4ae62
ehabyasser <ehabyasser344@gmail.com>
2023-07-23 15:30:16 +0300

    Update HTTPClient.swift

--- Sources/Network/HTTPClient.swift
+++ Sources/Network/HTTPClient.swift
@@ -13,29 +13,27 @@ public typealias CompletionHandler<T: Decodable> = (Result<T, HTTPError>) -> Voi
 
 public class HTTPClient {
     public static let shared = HTTPClient()
-    private let queue = OperationQueue()
     public func request<T: Decodable>(url: String,
                                       method: HttpMethod = .get,
                                       headers: [String: String]? = nil,
                                       body: Data? = nil,
                                       completion: @escaping CompletionHandler<T>) {
         DispatchQueue.global().async {
-            let networkOperation = NetworkOperation()
             let requestOperation = RequestOperation<T>(url: url , method: method , headers: headers , body: body , completion: completion)
-            networkOperation.completionBlock = {
-                if networkOperation.isConnected {
-                    requestOperation.start()
-                }else{
-                    if #available(iOS 13.0, *) {
-                        ToastBanner.shared.show(message: "No Internet connection", style: .error, position: .Bottom)
-                    } else {
-                        print("no internet connection")
-                    }
-                    completion(.failure(.NoInternet))
-                    requestOperation.cancel()
-                }
-            }
-            networkOperation.start()
+//            networkOperation.completionBlock = {
+//                if networkOperation.isConnected {
+//                    requestOperation.start()
+//                }else{
+//                    if #available(iOS 13.0, *) {
+//                        ToastBanner.shared.show(message: "No Internet connection", style: .error, position: .Bottom)
+//                    } else {
+//                        print("no internet connection")
+//                    }
+//                    completion(.failure(.NoInternet))
+//                    requestOperation.cancel()
+//                }
+//            }
+            requestOperation.start()
 //            requestOperation.addDependency(networkOperation)
 //            self.queue.addOperations([networkOperation , requestOperation], waitUntilFinished: true)
         }
