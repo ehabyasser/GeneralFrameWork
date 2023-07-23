f3a170081d551f63583ab26d336f370e1eddefbf
ehabyasser <ehabyasser344@gmail.com>
2023-07-23 18:05:32 +0300

    Update HTTPClient.swift

--- Sources/Network/HTTPClient.swift
+++ Sources/Network/HTTPClient.swift
@@ -17,8 +17,17 @@ public class HTTPClient {
                                       headers: [String: String]? = nil,
                                       body: Data? = nil,
                                       completion: @escaping CompletionHandler<T>) {
-         let requestOperation = RequestOperation<T>(url: url , method: method , headers: headers , body: body , completion: completion)
-        self.queue.addOperations([requestOperation], waitUntilFinished: false)
+        DispatchQueue.global().async {
+            let networkOperation = NetworkOperation()
+            let requestOperation = RequestOperation<T>(url: url , method: method , headers: headers , body: body , completion: completion)
+            requestOperation.addDependency(networkOperation)
+            self.queue.addOperations([networkOperation , requestOperation], waitUntilFinished: true)
+        }
     }
     
 }
+
+
+
+
+
