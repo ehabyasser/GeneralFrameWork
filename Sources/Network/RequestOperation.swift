42d49cccbce175b9e241c5fac7b2c5390bf8b5f6
ehabyasser <ehabyasser344@gmail.com>
2023-07-23 18:21:00 +0300

    Update RequestOperation.swift

--- Sources/Network/RequestOperation.swift
+++ Sources/Network/RequestOperation.swift
@@ -21,7 +21,6 @@ class RequestOperation<T:Decodable>:Operation {
         self.headers = headers
         self.body = body
         self.completion = completion
-        
     }
     
    
@@ -32,13 +31,12 @@ class RequestOperation<T:Decodable>:Operation {
     
     public func request(completion: @escaping CompletionHandler<T>) {
         if !NetworkManager.shared.isConnected {
-            completion(.failure(.NoInternet))
             if #available(iOS 13.0, *) {
                 DispatchQueue.main.async {
                     ToastBanner.shared.show(message: "Check your internet connection.", style: .error, position: .Bottom)
                 }
             } else {
-                print("Check your internet connection.")
+                completion(.failure(.NoInternet))
             }
             return
         }
