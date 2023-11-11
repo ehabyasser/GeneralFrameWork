fa518488ffd4be34c5182c42e66795ba327eaefa
ehabyasser <ehabyasser344@gmail.com>
2023-11-11 14:47:55 +0300

    Update Logger.swift

--- Sources/Utilities/Logger.swift
+++ Sources/Utilities/Logger.swift
@@ -41,11 +41,11 @@ open class Logger: NSObject {
     
     static func log(_ level:Level , message:Any){
         if message is Codable {
-            print(level.toIcon() , (message as? Codable)?.convertToString ?? "")
+            print(level.toIcon() , ((message as? Codable)?.convertToString ?? "").replacingOccurrences(of: "\"", with: "", options: NSString.CompareOptions.literal, range:nil))
         }else if message is String {
-            print(level.toIcon() , (message as? String) ?? "")
+            print(level.toIcon() , ((message as? String) ?? "").replacingOccurrences(of: "\"", with: "", options: NSString.CompareOptions.literal, range:nil))
         }else {
-            debugPrint(level.toIcon() , message)
+            debugPrint(level.toIcon() , message.replacingOccurrences(of: "\"", with: "", options: NSString.CompareOptions.literal, range:nil))
         }
     }
 }
