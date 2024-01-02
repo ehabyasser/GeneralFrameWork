8e96c23fd0a38601d2fbb5be0a549ca5e09d7513
ehabyasser <ehabyasser344@gmail.com>
2024-01-02 20:59:52 +0300

    Update RequestOperation.swift

--- Sources/Network/RequestOperation.swift
+++ Sources/Network/RequestOperation.swift
@@ -149,6 +149,7 @@ class RequestOperation<T:Decodable>:Operation {
             return model
         } catch {
             completion(.failure(RequestError(httpError: .jsonParsingFailure, data: nil)))
+            print(error.localizedDescription)
         }
         return nil
     }
